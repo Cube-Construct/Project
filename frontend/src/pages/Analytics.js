a262d76529f6b969e079d2e59eaa3e62a26872e3
vaishnavi802 <vaishnavi.kulkarni@walchandsangli.ac.in>
2023-08-12 11:52:29 +0530

    added:report

--- frontend/src/pages/Analytics.js
+++ frontend/src/pages/Analytics.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
 const Analytics = () => {
-    return (
-        <div className="bigBlurText">
-            Report
+  return (
+    <div className="">
+      <div className='main-body' style={{ backgroundColor: 'white' }}>
+        <div className="container">
+          <div className="title">Report</div>
+          <div className="content">
+            <div className='div-form'>
+              <div className="user-details">
+                <div className="input-box">
+                  <div className="details">Start date</div>
+                  <input type="date" />
+                </div>
+                <div className="input-box">
+                  <div className="details">End date</div>
+                  <input type="date" />
+                </div>
+              </div>
+              <button className="btn"
+                style={{ backgroundColor: "#222E3C", color: "white", border: "none", borderRadius: "5px", cursor: "pointer", float: "left" }}
+                title='Generate'>
+                Generate
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Analytics;
